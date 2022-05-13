bb582bd710d761788cc5e9f503f45bd26e4ca51d
avianz37 <65418581+avianz37@users.noreply.github.com>
2022-05-13 10:54:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -174,7 +174,7 @@ global.dmenub2 = '┃' //body for info cmd on Default menu
 global.dmenuf = '╰─●' //footer
 
 // COMMAND MENU
-global.dashmenu = '➦'
+global.dashmenu = '➦DASHBOARD'
 global.cmenut = '╭─●' //top
 global.cmenuh = '●' //header
 global.cmenub = '┃⬤' //body
