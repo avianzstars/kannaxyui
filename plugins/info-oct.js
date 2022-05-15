5f52c173e5fbe392e82a4239973c708cbca75ebb
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-05-15 13:03:47 +0700

    Update info-oct.js

--- plugins/info-oct.js
+++ plugins/info-oct.js
@@ -1,15 +1,16 @@
 import fs from 'fs'
 let handler = async (m, { conn, args, command }) => {
-let totalf = Object.values(global.plugins).filter(
+let oct = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `IG: yusuf.oct\n`, botdate + '\n\n' + wm, giflogo, [['Follow', sig]], m, {
+ await conn.sendButton(m.chat, `IG: yusuf.oct\n`, botdate + '\n\n' + wm, giflogo, [['Follow', '.oct']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: bottime,
-                        body: 'Total Cintaku Padamu',          previewType: 0,
+                        title: author,
+                        body: 'Jangan lupa Follow',
+                        previewType: 0,
                         thumbnail: fs.readFileSync("./oct.jpg"),
                         sourceUrl: sig
                       }}
