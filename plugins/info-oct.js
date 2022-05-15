d797a725a652179a8da0df15c3027b72b06c1fe6
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-05-15 12:48:14 +0700

    Update info-oct.js

--- plugins/info-oct.js
+++ plugins/info-oct.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `IG: yusuf.oct\n`, botdate + '\n\n' + wm, giflogo, [['Oct','oct']], m, {
+ await conn.sendButton(m.chat, `IG: yusuf.oct\n`, botdate + '\n\n' + wm, giflogo, [['Follow', sig]], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
                         body: 'Total Cintaku Padamu',          previewType: 0,
-                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
+                        thumbnail: fs.readFileSync("./oct.jpg"),
                         sourceUrl: sig
                       }}
 })
@@ -19,5 +19,5 @@ contextInfo: { externalAdReply :{
 
 handler.help = ['oct']
 handler.tags = ['info']
-handler.command = ['0ct']
+handler.command = ['oct']
 export default handler
