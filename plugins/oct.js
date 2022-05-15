b215c812f576a5d1828513ac7b6c016ae46bc30b
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-05-15 10:05:46 +0700

    Create oct.js

---
+++ plugins/oct.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+
+let oct = {
+    	oct: 'oct'
+}[type]
+if (msgg) return conn.sendButton(m.chat, oct, global.wm, null, [['Oct', '/oct']], m)}
+
+handler.help = ['oct']
+handler.tags = ['oct']
+handler.command = /^(oct)$/i
+
+export default handler
