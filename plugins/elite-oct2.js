61342c85e7fa6de6e448c33ecdb16413dab0bf66
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-05-16 09:29:36 +0700

    Create elite-oct2.js

---
+++ plugins/elite-oct2.js
@@ -0,0 +1,31 @@
+import fs from 'fs'
+let handler = async (m, { conn, args, command }) => {
+let oct = Object.values(global.plugins).filter(
+    (v) => v.help && v.tags
+  ).length;
+    
+let text = `╭─● *Biodata*
+┃● *Nama*: Yusuf
+┃● *Tanggal lahir*: Kepo
+┃● *Alamat*: Isekai
+┃● *Status*: Not responding
+╰─●`
+
+let inputPath = './banner.png'
+const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './oct.jpg')
+await conn.sendHydrated(m.chat, text, botdate + '\n\n' + wm, inputPath, global.sig, 'Follow', 'wa.me/628387315706', 'WA Owner', [
+      //['▣ VERIFY ▣', '/daftar'],
+      //[null,null],
+      //[null, null]
+    ], m)
+}
+
+let m1 = global.dmenut
+let m2 = global.dmenub
+let m3 = global.dmenub2
+let m4 = global.dmenuf
+
+handler.help = ['oct2']
+handler.tags = ['elite']
+handler.command = ['oct2']
+export default handler
