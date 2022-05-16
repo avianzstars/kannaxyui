d88422ede54447954132bb31c24422462b380562
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-05-16 08:37:34 +0700

    Update elite-oct.js

--- plugins/elite-oct.js
+++ plugins/elite-oct.js
@@ -11,7 +11,7 @@ let text = `╭─● *Instagram*
 
 let inputPath = './banner.png'
 const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './oct.jpg')
-await conn.sendButton(m.chat, text, botdate + '\n\n' + wm, inputPath, [['Follow', '.oct']], m, {
+await conn.sendButton(m.chat, text, botdate + '\n\n' + wm, inputPath, [['Donasi', '.donasi']], m, {
 contextInfo: { externalAdReply :{
                         description: stickauth,
                         title: author,
