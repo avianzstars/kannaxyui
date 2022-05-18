8856581e7c00d590d5efd367f5f5c7e30cc75d1b
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-05-18 07:11:01 +0700

    Update anime-neko.js

--- plugins/anime-neko.js
+++ plugins/anime-neko.js
@@ -1,10 +1,18 @@
 import fetch from 'node-fetch'
+import fs from 'fs'
 
 let handler = async (m, { conn }) => {
 	let ne = await (await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')).text()
     let nek = ne.split('\n')
     let neko = pickRandom(nek)
-	conn.sendButton(m.chat, 'Neko + poi = ?', wm, neko, [['Lagi kak','.neko']],m)
+	//conn.sendButton(m.chat, 'Neko + poi = ?', wm, neko, [['Lagi kak','.neko']],m)
+    conn.sendButton(m.chat, 'Neko + poi + ?', botdate + '\n\n' + wm, neko, [['Mau lagi', `.neko`]], m, {
+contextInfo: { externalAdReply :{
+                        description: stickauth,
+                        title: author,
+                        body: 'Jangan lupa Follow',
+                        thumbnail: fs.readFileSync("./oct.jpg"),
+                        sourceUrl: sig }}})
 }
 handler.command = /^(neko)$/i
 handler.tags = ['anime']
