163b114ae5cd1e708b1b8cf6bcbb854915688e3f
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-10 05:31:01 +0700

    Update anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -19,6 +19,5 @@ handler.help = ['waifu']
 handler.tags = ['anime']
 handler.command = /^(waifu)$/i
 handler.limit = true
-handler.private = true
 //MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
 export default handler
