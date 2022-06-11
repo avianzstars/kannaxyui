9695f9d2b7628ca58032a06a2074a719b9f8540a
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-11 07:52:17 +0700

    Update elite-oct.js

--- plugins/elite-oct.js
+++ plugins/elite-oct.js
@@ -14,13 +14,50 @@ let text = `╭─● *Owner*
 
 let inputPath = './banner.png'
 const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './oct.jpg')
-await conn.sendButton(m.chat, text, botdate + '\n\n' + wm, inputPath, [['Menu', '.menu']/*, ['Biodata','.oct2']*/], m, {
+/*await conn.sendButton(m.chat, text, botdate + '\n\n' + wm, inputPath, [['Menu', '.menu']/*, ['Biodata','.oct2']*/], m, {
 contextInfo: { externalAdReply :{
                         description: stickauth,
                         title: author,
                         body: 'Jangan lupa Follow',
                         thumbnail: fs.readFileSync("./oct.jpg"),
-                        sourceUrl: sig }}}) }
+                        sourceUrl: sig }}}) }*/
+
+const buttonMessage={
+    'document':{'url':sgc},
+    'mimetype':"application/pdf",
+    'fileName':global['wm'],
+    'fileLength':fsizedoc,
+    'pageCount':fpagedoc,
+    'contextInfo':{'forwardingScore':100,'isForwarded':true,
+    'externalAdReply':{
+        'mediaUrl':global['sig'],
+        'mediaType':2,
+        'previewType':"tes",
+        'title':global['titlebot'],
+        'body':global['titlebot'],
+        'thumbnail':fs.readFileSync("./banner.png"),
+        'sourceUrl':sgc}},
+        'caption':'Yusuf Expert',
+        'footer':text + '\n\n' +botdate + '\n\n' + wm,
+        'buttons':[
+            {
+            'buttonId':'.owner',
+            'buttonText':{
+            'displayText':'Owner'},
+            'type':1},
+            {
+            'buttonId':'.ping',
+            'buttonText':{
+            'displayText':'Ping'},
+            'type':1},
+            {
+            'buttonId':'.donasi',
+            'buttonText':{
+            'displayText':'Donasi'},
+            'type':1}],
+            'headerType':"Document"};
+                
+await conn.sendMessage(m.chat,buttonMessage,{'quoted':m,'mentionedJid':[m.sender]});}
 
 let m1 = global.dmenut
 let m2 = global.dmenub
