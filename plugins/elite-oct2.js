a5a58e0752f2f0ac0d36c66594c7fed40e07b7f2
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-11 10:08:55 +0700

    Update elite-oct2.js

--- plugins/elite-oct2.js
+++ plugins/elite-oct2.js
@@ -14,34 +14,32 @@ let text = `╭─● *Owner*
 let inputPath = './banner.png'
 const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './oct.jpg')
 
-const yusufMsg = {
+const yusufMsg={
     'document':{'url':sgc},
     'mimetype':'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    'fileName':'Yusuf Expert',
+    'fileName':global['wm'],
     'fileLength':fsizedoc,
     'pageCount':fpagedoc,
     'contextInfo':{
-    'forwardingScore':100,
-    'isForwarded':true,
-    'externalAdReply':{
-        'mediaUrl':global['sig'],
-        'mediaType':2,
-        'previewType':"Tes",
-        'title':global['titlebot'],
-        'body':global['titlebot'],
-        'thumbnail':fs.readFileSync("./banner.png"),
-        'sourceUrl':sgc}},
-        'caption':'➦ *OWNER*',
-        'footer':text + '\n\n' + wm,
-        'buttons': [
-            {
-            'buttonId':'.owner',
-            'buttonText':{
-            'displayText':'Owner'},
-            'type':1
-            }
-            ],
-            'headerType':"Tes"};
+        'forwardingScore':100,
+        'isForwarded':true,
+        'externalAdReply':{
+            'mediaUrl':sig,
+            'mediaType':2,
+            'previewType':'Yusuf',
+            'title':wm,
+            'body':wm,
+            'thumbnail':fs.readFileSync("./banner.png"),
+            'sourceUrl':sgc}},
+            'caption':'*OWNER*',
+            'footer':text+'\n\n'+botdate,
+            'buttons':[
+                {'buttonId':'.menu',
+                'buttonText':{
+                    'displayText':'Menu'},
+                    'type':1}
+                       ],
+                'headerType':'Yusuf'};
                 
 await conn.sendMessage(m.chat,yusufMsg,{'quoted':m,'mentionedJid':[m.sender]});
 }
