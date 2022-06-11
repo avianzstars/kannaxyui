7bc9593849257e10c55a258c210bf84e79deb9d6
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-11 07:57:22 +0700

    Update elite-oct.js

--- plugins/elite-oct.js
+++ plugins/elite-oct.js
@@ -22,7 +22,7 @@ contextInfo: { externalAdReply :{
                         thumbnail: fs.readFileSync("./oct.jpg"),
                         sourceUrl: sig }}}) }*/
 
-const buttonMessage = {
+let buttonMessage = {
     'document':{'url':sgc},
     'mimetype':"application/pdf",
     'fileName':global['wm'],
