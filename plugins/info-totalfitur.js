51052bbe24ff5849f81e17286e8aed31fe48ff85
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-14 04:41:35 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -5,13 +5,15 @@ let totalf = Object.values(global.plugins).filter(
   ).length;
  await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, fs.readFileSync("./Project Pemuda.pdf"), [['MENU','.menu']], m, {
 contextInfo: { externalAdReply :{
-                        mediaUrl: sig,
-                        mediaType: 2,
-                        description: 'anu',
-                        title: bottime,
-                        body: 'Total Cintaku Padamu',          previewType: 0,
-                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: 'https://wa.me/6283873115706'
+                        'mediaUrl':sig,
+                        'mediaType':2,
+                        'previewType':'Yusuf',
+                        'title':wm,
+                        'body':'Created By Yusuf',
+                        'thumbnail':fs.readFileSync("./banner.png"),
+                        'sourceUrl':'https://wa.me/6283873115706'}},
+                        //'caption':'*OWNER*',
+                        'footer':text+'\n\n'+botdate+'\n\n'+wm
                       }}
 })
 }
