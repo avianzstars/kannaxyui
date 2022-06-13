611de5a551e65e77e575cbdd994e00e0e5a80a12
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-14 04:33:12 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -5,7 +5,7 @@ let totalf = Object.values(global.plugins).filter(
   ).length;
  await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, fs.readFileSync("./Project Pemuda.pdf"), [['MENU','.menu']], m, {
 contextInfo: { externalAdReply :{
-                        mediaUrl: 'sig',
+                        mediaUrl: sig,
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
