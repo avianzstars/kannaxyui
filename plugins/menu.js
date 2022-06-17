acd05ccf7be2b79ac01f2be4228d24b8ed458355
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-18 04:57:20 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -456,49 +456,6 @@ const listMessage = {
                 
 conn.sendMessage(m.chat,yusufMsg,{'quoted':m,'mentionedJid':[m.sender]});
 
-const yusufMsg2={
-    'document':{'url':d4},
-    'mimetype':'application/pdf',
-    'fileName':'Owner Ganteng',
-    'fileLength':1000000000,
-    'pageCount':1,
-    'mediaType':2,
-    'contextInfo':{
-        'forwardingScore':0,
-        'isForwarded':false,
-        'externalAdReply':{
-            'showAdAttribution': true,
-            'mediaUrl':'https://wa.me/6283873115706',
-            'mediaType':1,
-            'previewType':'Yusuf',
-            'title':'Halo kak👋',
-            'body':'Afakah antum Wibu?',
-            'thumbnail':fs.readFileSync("./oct.jpg"),
-            'sourceUrl':'https://wa.me/6283873115706'}},
-            'caption':'*[ ! ] Klik File PDF*',
-            'footer':wm,
-            'buttons':[
-                {'buttonId':'.waifu',
-                'buttonText':{
-                    'displayText':'Waifu'},
-                    'type':1},
-                {'buttonId':'.loli',
-                'buttonText':{
-                    'displayText':'Loli'},
-                    'type':2},
-                {'buttonId':'.neko',
-                'buttonText':{
-                    'displayText':'Neko'},
-                    'type':3},
-                {'buttonId':'...',
-                'buttonText':{
-                    'displayText':'\n\n*Owner Ganteng Banget( ꈍᴗꈍ)*'},
-                    'type':4}
-                       ],
-                'headerType':'Yusuf'};
-                
-conn.sendMessage(m.chat,yusufMsg2,{'quoted':m,'mentionedJid':[m.sender]});
-
     //-------DOC TEMPLATE
     /*const message = {
             document: { url: thumb },
