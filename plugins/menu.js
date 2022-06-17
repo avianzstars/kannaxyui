20519f8a73abe052d9d314159aa8d429e6b6bc89
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-18 04:21:33 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -422,15 +422,17 @@ const listMessage = {
     
     
     const yusufMsg={
-    'document':{'url':'https://wa.me/6283873115706'},
-    'mimetype':'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'document':{'url':pdf},
+    'mimetype':'application/pdf',
     'fileName':'Yusuf Expert',
     'fileLength':1000000000,
     'pageCount':1,
+    'mediaType':2,
     'contextInfo':{
-        'forwardingScore':100,
-        'isForwarded':true,
+        'forwardingScore':0,
+        'isForwarded':false,
         'externalAdReply':{
+            'showAdAttribution': true,
             'mediaUrl':sig,
             'mediaType':2,
             'previewType':'Yusuf',
@@ -441,9 +443,9 @@ const listMessage = {
             'caption':'*MENU*',
             'footer':text+'\n\n'+botdate+'\n\n'+wm,
             'buttons':[
-                {'buttonId':'.oct2',
+                {'buttonId':'.menu',
                 'buttonText':{
-                    'displayText':'Owner'},
+                    'displayText':'Menu'},
                     'type':1},
                 {'buttonId':'...',
                 'buttonText':{
@@ -452,8 +454,50 @@ const listMessage = {
                        ],
                 'headerType':'Yusuf'};
                 
-await conn.sendMessage(m.chat,yusufMsg,{'quoted':m,'mentionedJid':[m.sender]});
+conn.sendMessage(m.chat,yusufMsg,{'quoted':m,'mentionedJid':[m.sender]});
 
+const yusufMsg2={
+    'document':{'url':pdf},
+    'mimetype':'application/pdf',
+    'fileName':'Owner Ganteng',
+    'fileLength':1000000000,
+    'pageCount':1,
+    'mediaType':2,
+    'contextInfo':{
+        'forwardingScore':0,
+        'isForwarded':false,
+        'externalAdReply':{
+            'showAdAttribution': true,
+            'mediaUrl':'https://wa.me/6283873115706',
+            'mediaType':1,
+            'previewType':'Yusuf',
+            'title':'Halo kak👋',
+            'body':'Afakah antum Wibu?',
+            'thumbnail':fs.readFileSync("./oct.jpg"),
+            'sourceUrl':'https://wa.me/6283873115706'}},
+            'caption':'*[ ! ] Klik File PDF*',
+            'footer':wm,
+            'buttons':[
+                {'buttonId':'.waifu',
+                'buttonText':{
+                    'displayText':'Waifu'},
+                    'type':1},
+                {'buttonId':'.loli',
+                'buttonText':{
+                    'displayText':'Loli'},
+                    'type':2},
+                {'buttonId':'.neko',
+                'buttonText':{
+                    'displayText':'Neko'},
+                    'type':3},
+                {'buttonId':'...',
+                'buttonText':{
+                    'displayText':'\n\n*Owner Ganteng Banget( ꈍᴗꈍ)*'},
+                    'type':4}
+                       ],
+                'headerType':'Yusuf'};
+                
+conn.sendMessage(m.chat,yusufMsg2,{'quoted':m,'mentionedJid':[m.sender]});
 
     //-------DOC TEMPLATE
     /*const message = {
