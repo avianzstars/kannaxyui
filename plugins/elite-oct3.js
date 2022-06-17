c140af7e44b4aa3f5669000e2f476f1fc0a0d16f
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-17 11:10:45 +0700

    Update elite-oct3.js

--- plugins/elite-oct3.js
+++ plugins/elite-oct3.js
@@ -17,61 +17,17 @@ let wa = './wa.jpg'
 let inputPath = './banner.png'
 const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './oct.jpg')
 
-const yusufMsg={
-    'document':{'url':pdf},
-    'mimetype':'application/pdf',
-    'fileName':'Yusuf Expert',
-    'fileLength':1000000000,
-    'pageCount':1,
-    'mediaType':2,
-    'contextInfo':{
-        'forwardingScore':0,
-        'isForwarded':false,
-        'externalAdReply':{
+conn.sendButton(m.chat, '*OWNER*', text+'\n\n'+botdate+'\n\n'+wm, pdf, [['Menu', '.menu']], m, {
+contextInfo: { externalAdReply :{
+            'showAdAttribution': true,
             'mediaUrl':sig,
             'mediaType':2,
             'previewType':'Yusuf',
             'title':wm,
             'body':'Created By Yusuf',
             'thumbnail':fs.readFileSync("./banner.png"),
-            'sourceUrl':'https://wa.me/6283873115706'}},
-            'caption':'*OWNER*',
-            'footer':text+'\n\n'+botdate+'\n\n'+wm,
-            'templateButtons': [
-                {
-                    'urlButton': {
-                        'displayText': `${namebot}`,
-                        'url': 'https://kannxapi.herokuapp.com/'
-                    }
-                },
-                {
-                    'urlButton': {
-                        'displayText': 'Group Official',
-                        'url': sgc
-                    }
-                },
-                {
-                    'quickReplyButton': {
-                        'displayText': 'Owner',
-                        'id': '.owner'
-                    }
-                },
-                {
-                    'quickReplyButton': {
-                        'displayText': 'Speed',
-                        'id': '.ping'
-                    }
-                },
-                {
-                    'quickReplyButton': {
-                        'displayText': 'Donasi',
-                        'id': '.donasi'
-                    }
-                },
-            ],
-                'headerType':'Yusuf'};
-                
-await conn.sendMessage(m.chat,yusufMsg,{'quoted':m,'mentionedJid':[m.sender]});
+            'sourceUrl':'https://wa.me/6283873115706' }}})
+
 }
 
 handler.help = ['oct3']
