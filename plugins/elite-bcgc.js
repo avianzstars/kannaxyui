64a03dbf35ae70e7eeb2056a1d65300a5351d27b
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-18 13:06:19 +0700

    Update elite-bcgc.js

--- plugins/elite-bcgc.js
+++ plugins/elite-bcgc.js
@@ -8,48 +8,16 @@ let handler = async (m, { conn, text }) => {
 	await m.reply(`_Mengirim pesan broadcast ke ${groups.length} group_`)
 	for (let id of groups) 
 
-const yusufMsg2={
-    'document':{'url':pdf},
-    'mimetype':'application/pdf',
-    'fileName':'Owner Ganteng',
-    'fileLength':1000000000,
-    'pageCount':1,
-    'mediaType':2,
-    'contextInfo':{
-        'forwardingScore':0,
-        'isForwarded':false,
-        'externalAdReply':{
+await conn.sendButton(id, teks, wm, pdf, [['Menu', '.menu']], m, {
+contextInfo: { externalAdReply :{
             'showAdAttribution': true,
-            'mediaUrl':'https://wa.me/6283873115706',
-            'mediaType':1,
+            'mediaUrl':sig,
+            'mediaType':2,
             'previewType':'Yusuf',
-            'title':'Halo kak👋',
-            'body':'Afakah antum Wibu?',
-            'thumbnail':fs.readFileSync("./oct.jpg"),
-            'sourceUrl':'https://wa.me/6283873115706'}},
-            'caption':'*[ ! ] Klik File PDF*',
-            'footer':wm,
-            'buttons':[
-                {'buttonId':'.waifu',
-                'buttonText':{
-                    'displayText':'Waifu'},
-                    'type':1},
-                {'buttonId':'.loli',
-                'buttonText':{
-                    'displayText':'Loli'},
-                    'type':2},
-                {'buttonId':'.neko',
-                'buttonText':{
-                    'displayText':'Neko'},
-                    'type':3},
-                {'buttonId':'...',
-                'buttonText':{
-                    'displayText':'\n\n*Owner Ganteng Banget( ꈍᴗꈍ)*'},
-                    'type':4}
-                       ],
-                'headerType':'Yusuf'};
-                
-await conn.sendMessage(id,yusufMsg2);
+            'title':wm,
+            'body':'Created By Yusuf',
+            'thumbnail':fs.readFileSync("./banner.png"),
+            'sourceUrl':'https://wa.me/6283873115706' }}})
 
 	//await conn.sendButton(id,'*—「 Broadcast 」—*\n' + teks, wm, [['⋮☰ Menu', '.menu'], ['Owner', '.owner']], m)
 	m.reply('Selesai Broadcast All Group')
