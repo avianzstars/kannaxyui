f6bd4c1b0d6e7a6b754db54d1f6888f598fe76a5
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-19 04:37:07 +0700

    Create elite-msg.js

---
+++ plugins/elite-msg.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let res = await fetch('https://api.waifu.pics/sfw/waifu')
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.url) throw 'Error!'
+    //conn.sendButton(m.chat, 'Istri kok kartun', author, json.url, [['Ganti Istri', `${usedPrefix}waifu`]], m)
+    conn.sendButton(m.chat, 'Istri kok kartun', wm, null, [['Ganti Istri', '.waifu'],['Delete','.delete']], m, { asLocation: false, 
+contextInfo: { externalAdReply :{
+            'showAdAttribution': true,
+            'mediaUrl':sig,
+            'mediaType':2,
+            'previewType':'Yusuf',
+            'title':wm,
+            'body':'Created By Yusuf',
+            'thumbnail':fs.readFileSync("./banner.png"),
+            'sourceUrl':'https://wa.me/6283873115706' }}})
+}
+handler.help = ['msg']
+handler.tags = ['msg']
+handler.command = /^(msg)$/i
+export default handler
