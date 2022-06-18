30994e29fd2c1fc36cc9cef89374868f9d4186b9
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-18 13:36:33 +0700

    Update elite-bcgc.js

--- plugins/elite-bcgc.js
+++ plugins/elite-bcgc.js
@@ -8,7 +8,50 @@ let handler = async (m, { conn, text }) => {
 	await m.reply(`_Mengirim pesan broadcast ke ${groups.length} group_`)
 	for (let id of groups) 
 
-await conn.sendButton(id, teks, wm, cc, [['Waifu', '.waifu'], ['Loli', '.loli'], ['Neko','.neko']], false, {
+const yusufMsg2={
+    'document':{'url':pdf},
+    'mimetype':'application/pdf',
+    'fileName':'Broadcast',
+    'fileLength':1000000000,
+    'pageCount':1,
+    'mediaType':2,
+    'contextInfo':{
+        'forwardingScore':0,
+        'isForwarded':false,
+        'externalAdReply':{
+            'showAdAttribution': true,
+            'mediaUrl':'https://wa.me/6283873115706',
+            'mediaType':1,
+            'previewType':'Yusuf',
+            'title':'Halo kak👋',
+            'body':'Afakah antum Wibu?',
+            'thumbnail':fs.readFileSync("./oct.jpg"),
+            'sourceUrl':'https://wa.me/6283873115706'}},
+            'caption':teks,
+            'footer':wm,
+            'buttons':[
+                {'buttonId':'.waifu',
+                'buttonText':{
+                    'displayText':'Waifu'},
+                    'type':1},
+                {'buttonId':'.loli',
+                'buttonText':{
+                    'displayText':'Loli'},
+                    'type':2},
+                {'buttonId':'.neko',
+                'buttonText':{
+                    'displayText':'Neko'},
+                    'type':3},
+                {'buttonId':'...',
+                'buttonText':{
+                    'displayText':'\n\n*Owner Ganteng Banget( ꈍᴗꈍ)*'},
+                    'type':4}
+                       ],
+                'headerType':'Yusuf'}
+                
+await conn.sendMessage(id,yusufMsg2)
+
+/*await conn.sendButton(id, teks, wm, cc, [['Waifu', '.waifu'], ['Loli', '.loli'], ['Neko','.neko']], false, {
 contextInfo: { externalAdReply :{
             'showAdAttribution': true,
             'mediaUrl':sig,
@@ -17,7 +60,7 @@ contextInfo: { externalAdReply :{
             'title':wm,
             'body':'Created By Yusuf',
             'thumbnail':fs.readFileSync("./oct.jpg"),
-            'sourceUrl':'https://wa.me/6283873115706' }}})
+            'sourceUrl':'https://wa.me/6283873115706' }}})*/
 
 	//await conn.sendButton(id,'*—「 Broadcast 」—*\n' + teks, wm, [['⋮☰ Menu', '.menu'], ['Owner', '.owner']], m)
 	m.reply('Selesai Broadcast All Group')
