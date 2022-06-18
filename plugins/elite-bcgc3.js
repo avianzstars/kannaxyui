6329a2b048fb869c2ae78ba2d71b89120cc38a60
anzavi753 <65418581+avianz37@users.noreply.github.com>
2022-06-19 06:26:30 +0700

    Update elite-bcgc3.js

--- plugins/elite-bcgc3.js
+++ plugins/elite-bcgc3.js
@@ -1,77 +1,35 @@
+import { randomBytes } from 'crypto'
 import fs from 'fs'
+
 let handler = async (m, { conn, text }) => {
-	let wm = global.wm
-        let pdf = './wa.pdf'
-	let groups = Object.entries(await conn.groupFetchAllParticipating()).filter(([jid, chat]) => !chat?.announce).map(v => v[0]),
-		cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
-		teks = text ? text : cc.text
-                cc2 = await m.getQuotedObj() : false || m
-const yusufMsg2={
-    'document':{'url':cc},
-    'mimetype':'IMAGE',
-    //'fileName':'Broadcast',
-    //'fileLength':1000000000,
-    //'pageCount':1,
-    //'mediaType':2,
-    'contextInfo':{
-        'forwardingScore':0,
-        'isForwarded':false,
-        'externalAdReply':{
-            'showAdAttribution': true,
-            'mediaUrl':'https://wa.me/6283873115706',
-            'mediaType':1,
-            'previewType':'Yusuf',
-            'title':'Halo kak👋',
-            'body':'Afakah antum Wibu?',
-            'thumbnail':fs.readFileSync("./oct.jpg"),
-            'sourceUrl':'https://wa.me/6283873115706'}},
-            'caption':teks,
-            'footer':wm,
-            'buttons':[
-                {'buttonId':'.waifu',
-                'buttonText':{
-                    'displayText':'Waifu'},
-                    'type':1},
-                {'buttonId':'.loli',
-                'buttonText':{
-                    'displayText':'Loli'},
-                    'type':2},
-                {'buttonId':'.neko',
-                'buttonText':{
-                    'displayText':'Neko'},
-                    'type':3},
-                {'buttonId':'...',
-                'buttonText':{
-                    'displayText':'\n\n*Owner Ganteng Banget( ꈍᴗꈍ)*'},
-                    'type':4}
-                       ],
-                'headerType':'Yusuf'}
-	await m.reply(`_Mengirim pesan broadcast ke ${groups.length} group_`)
-	for (let id of groups) 
+  let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
+  let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+  let teks = text ? text : cc.text
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+  for (let id of groups)
 
-//await conn.sendMessage(m.chat,yusufMsg2)
-
-await conn.sendButton(m.chat, teks, wm, cc, [['⋮☰ Menu', '.menu'], ['Owner', '.oct2']], false, {
+await conn.sendButton(m.chat, teks, wm, cc, [['Waifu', '.waifu'], ['Loli', '.loli'], ['Neko','.neko']], false, {
 contextInfo: { externalAdReply :{
             'showAdAttribution': true,
-            //'mediaUrl':sig,
-            //'mediaType':1,
-            //'previewType':'Yusuf',
+            'mediaUrl':sig,
+            'mediaType':1,
+            'previewType':'Yusuf',
             'title':wm,
             'body':'Created By Yusuf',
             'thumbnail':fs.readFileSync("./oct.jpg"),
-            'sourceUrl':'' }}})
+            'sourceUrl':'https://wa.me/6283873115706' }}})
 
-	//await conn.sendButton(id,'*—「 Broadcast 」—*\n' + teks, wm, [['⋮☰ Menu', '.menu'], ['Owner', '.owner']], m)
-	m.reply('Selesai Broadcast All Group')
+  m.reply('Selesai Broadcast All Group :)')
 }
-handler.help = ['bcgc3']
-handler.command = /^(bcgc3)$/i
+handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
+handler.command = /^(bcgc3)$/i
+
 handler.owner = true
 
 export default handler
 
-function readMore() {
-	return (String.fromCharCode(8206)).repeat(4001)
-}
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
